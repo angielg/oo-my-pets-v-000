001f155c3c5e4859963e29b406081800bdd38507
Angie Goza <angielg9888@gmail.com>
2019-08-15 17:15:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -74,6 +74,6 @@ class Owner
   end
   
   def list_pets
-    puts "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
+    return "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
   end
 end
